f2e89bcb72f0f1a2b9e3da8112e0dd9702a50261
Andrieu, Alban <alban.andrieu@misys.com>
2017-11-21 23:06:55 +0100

    disable windows specific

--- gui/src/test/java/com/nabla/project/visma/selenium/tests/helper/WindowsRegistry.java
+++ gui/src/test/java/com/nabla/project/visma/selenium/tests/helper/WindowsRegistry.java
@@ -46,16 +46,16 @@ import org.openqa.selenium.os.WindowsUtils;
 public class WindowsRegistry
 {
     // @Test
-    public void testReadRegistry()
-    {
-        final String osname = WindowsUtils.readStringRegistryValue("HKEY_LOCAL_MACHINE\\SOFTWARE\\Microsoft\\Windows NT\\CurrentVersion\\ProductName");
-        System.out.println(osname);
-    }
-
+    // public void testReadRegistry()
+    // {
+    // final String osname = WindowsUtils.readStringRegistryValue("HKEY_LOCAL_MACHINE\\SOFTWARE\\Microsoft\\Windows NT\\CurrentVersion\\ProductName");
+    // System.out.println(osname);
+    // }
+    //
     // @Test
-    public void testWriteRegistry()
-    {
-        WindowsUtils.writeStringRegistryValue("HKEY_CURRENT_USER\\SOFTWARE\\Selenium\\SeleniumVersion", "2.24");
-        Assert.assertEquals("2.24", WindowsUtils.readStringRegistryValue("HKEY_CURRENT_USER\\SOFTWARE\\Selenium\\SeleniumVersion"));
-    }
+    // public void testWriteRegistry()
+    // {
+    // WindowsUtils.writeStringRegistryValue("HKEY_CURRENT_USER\\SOFTWARE\\Selenium\\SeleniumVersion", "2.24");
+    // Assert.assertEquals("2.24", WindowsUtils.readStringRegistryValue("HKEY_CURRENT_USER\\SOFTWARE\\Selenium\\SeleniumVersion"));
+    // }
 }
