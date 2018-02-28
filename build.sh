e35e68f9266371b002b5ed9c4d49a2a888281808
Andrieu, Alban <alban.andrieu@misys.com>
2018-02-28 22:12:18 +0100

    Fix java.io.tmpdir

--- build.sh
+++ build.sh
@@ -3,6 +3,9 @@ set -xv
 
 ./clean.sh
 
-mvn clean install -Dserver=jetty9x
+#export JAVA_OPTS=""
+
+mvn clean install -Dserver=jetty9x -Darquillian=arq-jetty-embedded
+#-Parq-jetty-embedded,jetty9x
 
 exit 0
