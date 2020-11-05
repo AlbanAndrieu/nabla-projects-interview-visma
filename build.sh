62b0c45429b0348f0e1c8c75d5d805e31ffd2c55
Alban Andrieu <alban.andrieu@free.fr>
2020-11-05 10:45:34 +0100

    Fix settings.xml

--- build.sh
+++ build.sh
@@ -5,8 +5,9 @@ set -xv
 
 #export JAVA_OPTS=""
 
-./mvnw clean install -Dserver=jetty9x -Darquillian=arq-jetty-embedded
-#-Parq-jetty-embedded,jetty9x
+#./mvnw clean install -Dserver=jetty9x -Darquillian=arq-jetty-embedded
+
+./mvnw clean install -Ddatabase=derby -Dserver=jetty9x -Darquillian=arq-jetty-embedded -Psample,jacoco,integration,jmeter,run-its,arq-jetty-embedded,!arq-weld-ee-embedded,!arq-jbossas-managed
 
 #java -jar gui/target/dependency/jetty-runner.jar --port 9090 gui/target/*.war
 
